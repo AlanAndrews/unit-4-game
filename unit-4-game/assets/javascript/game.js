7dc0c4825f16a38e763cf2e935df3f1be21aac25
Alan <andrewsalan88@gmail.com>
2018-04-27 19:49:32 -0500

    edited CSS, got rid of wins and losses functions and coded each crystal button

--- unit-4-game/assets/javascript/game.js
+++ unit-4-game/assets/javascript/game.js
@@ -13,58 +13,75 @@ $(document).ready(function(){
     // $("#randomNum").text(randomNum);
     // $("#userScore").text(NumToGuess);
    
-    function wins() {
-        alert("sup");
-        wins++;
-        $("#wins").text(wins);
-        initializeGame();
-    }
+    // function wins() {
+    //     alert("sup");
+    //     wins++;
+    //     $("#wins").text(wins);
+    //     initializeGame();
+    // }
 
-    function losses() {
-        alert("sup");
-        losses++;
-        $("#losses").text(losses);
-        initializeGame();
-    }
+    // function losses() {
+    //     alert("sup");
+    //     losses++;
+    //     $("#losses").text(losses);
+    //     initializeGame();
+    // }
 
     function initializeGame() {
         randomNum = Math.floor(Math.random() * 121) + 19;
         $("#randomNum").text(randomNum);
-        console.log(randomNum);
+        // console.log(randomNum);
         xtalOne = Math.floor(Math.random() * 12) + 1;
         xtalTwo = Math.floor(Math.random() * 12) + 1;
         xtalThree = Math.floor(Math.random() * 12) + 1;
         xtalFour = Math.floor(Math.random() * 12) + 1;
-        console.log(xtalOne);
-        console.log(xtalTwo);
-        console.log(xtalThree);
-        console.log(xtalFour);
+        userNum = 0;
+        // console.log(xtalOne);
+        // console.log(xtalTwo);
+        // console.log(xtalThree);
+        // console.log(xtalFour);
 
     }
 
     $("#crystalOne").on("click", function(){
         userNum = userNum + xtalOne;
         console.log(userNum)
-
-        // $("#userNumber").text(userNum);
+        $("#userNumber").text(userNum);
         if (userNum == randomNum) {
-            wins();
+            $("#wins").text("Wins: " + wins);
+            initializeGame();
+            // wins();
+
         }
         else if (userNum > randomNum) {
-            losses();
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            initializeGame();
+            // losses();
         }
+        // if (userNum == randomNum) {
+        //     wins();
+        // }
+        // else if (userNum > randomNum) {
+        //     losses();
+        // }
     });
 
     $("#crystalTwo").on("click", function(){
         userNum = userNum + xtalTwo;
         console.log(userNum)
-
-        // $("#userNumber").text(userNum);
+        $("#userNumber").text(userNum);
         if (userNum == randomNum) {
-            wins();
+            $("#wins").text("Wins: " + wins);
+            initializeGame();
+            // wins();
+
         }
         else if (userNum > randomNum) {
-            losses();
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            initializeGame();
+            // losses();
         }
     });
 
@@ -72,27 +89,49 @@ $(document).ready(function(){
     $("#crystalThree").on("click", function(){
         userNum = userNum + xtalThree;
         console.log(userNum)
-
-        // $("#userNumber").text(userNum);
+        $("#userNumber").text(userNum);
         if (userNum == randomNum) {
-            wins();
+            $("#wins").text("Wins: " + wins);
+            initializeGame();
+            // wins();
+
         }
         else if (userNum > randomNum) {
-            losses();
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            initializeGame();
+            // losses();
         }
+        // if (userNum == randomNum) {
+        //     wins();
+        // }
+        // else if (userNum > randomNum) {
+        //     losses();
+        // }
     });
 
     $("#crystalFour").on("click", function(){
         userNum = userNum + xtalFour;
         console.log(userNum)
-
-        // $("#userNumber").text(userNum);
+        $("#userNumber").text(userNum);
         if (userNum == randomNum) {
-            wins();
+            $("#wins").text("Wins: " + wins);
+            initializeGame();
+            // wins();
+
         }
         else if (userNum > randomNum) {
-            losses();
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            initializeGame();
+            // losses();
         }
+        // if (userNum == randomNum) {
+        //     wins();
+        // }
+        // else if (userNum > randomNum) {
+        //     losses();
+        // }
     });
     
 });
